d20632114db138f6e5a9a0ce026f6fa2048d0d2e
Omar <account1@fulls-mbp.attlocal.net>
2023-10-15 22:18:09 -0700

    Added drawer and icons buttons

--- src/components/employeeList.jsx
+++ src/components/employeeList.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { list } from "../mockData";
 import "../styling/employeeList.scss";
+import DrawerComponent from "./drawer";
+import IconButton from "@mui/material/IconButton";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const EmployeeList = () => {
   return (
     <div>
+      <DrawerComponent />
       {list.map((item) => {
         return (
           <div id="container">
             <span>{item.fullName}</span>
             <span>{item.address}</span>
             <span>{item.cellNumber}</span>
+            <span>
+              <IconButton>
+                <EditNoteIcon />
+              </IconButton>
+              <IconButton>
+                <DeleteForeverIcon />
+              </IconButton>
+            </span>
           </div>
         );
       })}
