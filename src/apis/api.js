1433e83910f703791d431f65eb69a6a0867b3cd8
Omar <account1@fulls-mbp.attlocal.net>
2023-10-24 00:46:30 -0700

    Added new api calls

---
+++ src/apis/api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { list } from "../mockData";
+
+export const getEmployeeList = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/api/allEmployees");
+    // console.log(response);
+    const { data = [] } = response || {};
+    return data;
+  } catch (error) {
+    console.error(error);
+    return list;
+  }
+};
+
+export const addNewEmployee = async (employee) => {
+  try {
+    const response = await axios.post("http://localhost:8080/api/add", {
+      ...employee,
+    });
+    // console.log(response);
+    const { data = [] } = response || {};
+    return data;
+  } catch (error) {
+    console.error(error);
+    return list;
+  }
+};
+
+export const deleteEmployeeFunc = async (id) => {
+  try {
+    const response = await axios.delete(
+      `http://localhost:8080/api/deleteEmployee/${id}`
+    );
+    const { data = [] } = response || {};
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
