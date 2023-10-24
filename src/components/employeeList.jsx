3a5863ec7466f182e6908f72ac9e21d0e6e7f180
Omar <account1@fulls-mbp.attlocal.net>
2023-10-24 00:46:59 -0700

    Updated layout of add employee and employee list

--- src/components/employeeList.jsx
+++ src/components/employeeList.jsx
@@ -1,32 +1,73 @@
-import React from "react";
-import { list } from "../mockData";
+import React, { useEffect, useState } from "react";
 import "../styling/employeeList.scss";
-import DrawerComponent from "./drawer";
+// import SidebarComponent from "./sidebar";
+import NavbarComponent from "./navbar";
 import IconButton from "@mui/material/IconButton";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { getEmployeeList } from "../apis/api";
+import { DialogComponent } from "./dialog";
+import { deleteEmployeeFunc } from "../apis/api";
 
 const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [deleteEmployee, setDeleteEmployee] = useState(false);
+  const [idxDelete, setIdxDelete] = useState(-1);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getEmployeeList();
+      setEmployees(data);
+    };
+    getData();
+  }, []);
+
+  const cancel = () => {
+    setDeleteEmployee(false);
+  };
+
+  const deleteCall = async () => {
+    setDeleteEmployee(false);
+    const item = employees[idxDelete];
+    await deleteEmployeeFunc(item.id);
+    const data = await getEmployeeList();
+    setEmployees(data);
+  };
+
   return (
-    <div>
-      <DrawerComponent />
-      {list.map((item) => {
-        return (
-          <div id="container">
-            <span>{item.fullName}</span>
-            <span>{item.address}</span>
-            <span>{item.cellNumber}</span>
-            <span>
-              <IconButton>
-                <EditNoteIcon />
-              </IconButton>
-              <IconButton>
-                <DeleteForeverIcon />
-              </IconButton>
-            </span>
-          </div>
-        );
-      })}
+    <div id="employee-list-container">
+      {/* <DrawerComponent /> */}
+      {/* <SidebarComponent /> */}
+      <NavbarComponent />
+      <DialogComponent
+        open={deleteEmployee}
+        cancel={cancel}
+        callDelete={deleteCall}
+      />
+      <div id="list-container">
+        {employees.map((item, index) => {
+          return (
+            <div className="list-items" key={`${item.fullName}_${index}`}>
+              <span>{item.fullName}</span>
+              <span>{item.address}</span>
+              <span>{item.cellNumber}</span>
+              <span>
+                <IconButton>
+                  <EditNoteIcon />
+                </IconButton>
+                <IconButton
+                  onClick={() => {
+                    setDeleteEmployee(true);
+                    setIdxDelete(index);
+                  }}
+                >
+                  <DeleteForeverIcon />
+                </IconButton>
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
