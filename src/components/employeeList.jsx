c7a985f31ca21b316497c646f07955ee7c314e31
Omar <account1@fulls-mbp.attlocal.net>
2023-10-25 00:06:08 -0700

    Updated parent component employee list to be able to open edit employee dialog and dialog to delete employee

--- src/components/employeeList.jsx
+++ src/components/employeeList.jsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "../styling/employeeList.scss";
-// import SidebarComponent from "./sidebar";
 import NavbarComponent from "./navbar";
 import IconButton from "@mui/material/IconButton";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { getEmployeeList } from "../apis/api";
 import { DialogComponent } from "./dialog";
-import { deleteEmployeeFunc } from "../apis/api";
+import { deleteEmployeeFunc, updateEmployeeFunc } from "../apis/api";
+import EditEmployeeComponenet from "./editEmployee";
+import { useEditEmployeeStore } from "../store/store";
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [deleteEmployee, setDeleteEmployee] = useState(false);
-  const [idxDelete, setIdxDelete] = useState(-1);
+  const [editEmployee, setEditEmployee] = useState(false);
+  const [selectedIdx, setSelectedIndex] = useState({});
+  const updateFullName = useEditEmployeeStore((state) => state.editFullName);
+  const updateCellNumber = useEditEmployeeStore(
+    (state) => state.editCellNumber
+  );
+  const updateAddress = useEditEmployeeStore((state) => state.editAddress);
+  const updateCity = useEditEmployeeStore((state) => state.editCity);
+  const updateZipcode = useEditEmployeeStore((state) => state.editZipcode);
+
+  const editFullName = useEditEmployeeStore((state) => state.fullName);
+  const editCellNumber = useEditEmployeeStore((state) => state.cellNumber);
+  const editAddress = useEditEmployeeStore((state) => state.address);
+  const editCity = useEditEmployeeStore((state) => state.city);
+  const editZipcode = useEditEmployeeStore((state) => state.zipcode);
 
   useEffect(() => {
     const getData = async () => {
@@ -28,10 +43,40 @@ const EmployeeList = () => {
 
   const deleteCall = async () => {
     setDeleteEmployee(false);
-    const item = employees[idxDelete];
+    const item = employees[selectedIdx.index];
     await deleteEmployeeFunc(item.id);
     const data = await getEmployeeList();
     setEmployees(data);
+  };
+
+  const openEditDialog = (id, index) => {
+    const row = employees[index];
+    updateFullName(row.fullName);
+    updateCellNumber(row.cellNumber);
+    updateAddress(row.address);
+    updateCity(row.city);
+    updateZipcode(row.zipCode);
+    setSelectedIndex({ id, index });
+    setEditEmployee(true);
+  };
+
+  const closeEditDialog = () => {
+    setEditEmployee(false);
+  };
+
+  const updateEmployee = async () => {
+    let currentEmployee = {
+      fullName: editFullName,
+      cellNumber: editCellNumber,
+      address: editAddress,
+      city: editCity,
+      zipcode: editZipcode,
+      id: selectedIdx.id,
+    };
+    await updateEmployeeFunc(currentEmployee);
+    const data = await getEmployeeList();
+    setEmployees(data);
+    setEditEmployee(false);
   };
 
   return (
@@ -44,6 +89,11 @@ const EmployeeList = () => {
         cancel={cancel}
         callDelete={deleteCall}
       />
+      <EditEmployeeComponenet
+        open={editEmployee}
+        closeDialog={closeEditDialog}
+        updateEmployee={updateEmployee}
+      />
       <div id="list-container">
         {employees.map((item, index) => {
           return (
@@ -52,13 +102,17 @@ const EmployeeList = () => {
               <span>{item.address}</span>
               <span>{item.cellNumber}</span>
               <span>
-                <IconButton>
+                <IconButton
+                  onClick={() => {
+                    openEditDialog(item.id, index);
+                  }}
+                >
                   <EditNoteIcon />
                 </IconButton>
                 <IconButton
                   onClick={() => {
+                    setSelectedIndex({ id: item.id, index });
                     setDeleteEmployee(true);
-                    setIdxDelete(index);
                   }}
                 >
                   <DeleteForeverIcon />
