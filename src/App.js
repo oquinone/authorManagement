75a22a238fcfdededb7bdd6875b675bd504d11ae
Omar <account1@omars-mbp.attlocal.net>
2024-06-04 14:24:47 -0700

    changed react router version and updated syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AuthorList from "./components/authorList";
+import AddAuthorComponent from "./components/addAuthor";
+// import ErrorPage from "./components/errorPage";
 
 function App() {
   return (
-    <div className="App">
-      <AuthorList />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <AuthorList />
+        </Route>
+        <Route path="/add" exact>
+          <AddAuthorComponent />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,54 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import {
-  // createBrowserRouter,
-  RouterProvider,
-  createHashRouter,
-  // HashRouter,
-  // Route,
-  // Routes,
-} from "react-router-dom";
-import AuthorList from "./components/authorList";
-import AddAuthorComponent from "./components/addAuthor";
-import ErrorPage from "./components/errorPage";
+import App from "./App";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <AuthorList />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/add",
-        element: <AddAuthorComponent />,
-        // loader: teamLoader,
-      },
-    ],
-  },
-  {
-    path: "/add",
-    element: <AddAuthorComponent />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
-    {/* <HashRouter baseline="/">
-      <Routes>
-        <Route path="/" Component={AuthorList} exact />
-        <Route path="/add" Component={AddAuthorComponent} exact />
-      </Routes>
-    </HashRouter> */}
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
