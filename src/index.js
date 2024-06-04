4296503a26a791c708707443c1c70b9526cd7a36
Omar <account1@omars-mbp.attlocal.net>
2024-06-04 13:18:28 -0700

    Trying to update hash router to work in production

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import ErrorPage from "./components/errorPage";
 
 const router = createHashRouter([
   {
-    path: "/*",
+    path: "/authorManagement",
     element: <AuthorList />,
     errorElement: <ErrorPage />,
   },
